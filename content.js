1646060c7ba4b61975095b265c69305ff8b3b766
olfMombach <olf.mombach@gmx.de>
2020-04-15 21:08:40 +0200

    Fuck dis shit

--- content.js
+++ content.js
@@ -1,27 +1,11 @@
-window.onload = function(e) {
-    let el = document.createElement("img");
-    el.id = "profilePicShower";
-    el.style = "display: none; position: absolute;";
-    document.body.appendChild(el);
+console.log("Started");
 
-    let profilePics = Array.prototype.slice.call(document.querySelectorAll("yt-img-shadow>img#img.style-scope.yt-img-shadow")).filter(x => {
-        return x.src.startswith("https://yt3.ggpht.com") && x.parentNode.id !== "hearted-thumbnail";
-    });
+let img = document.createElement("img");
+img.style.display = "none";
+img.style.position = "absolute";
+img.id = "profilePicMover";
+document.appendChild(img);
 
-    for (let target of profilePics){
-        target.addEventListener("mousemove", e => {
-            let x = e.clientX;
-            let y = e.clientY;
-            let mover = document.getElementById("mover");
-            mover.style.left = x + 40 + "px";
-            mover.style.top = y + 40 + "px";
-        });
-        target.addEventListener("mouseenter", e => {
-            document.getElementById("mover").style.display = "unset";
-            this.console.log(e);
-        });
-        target.addEventListener("mouseleave", e => {
-            document.getElementById("mover").style.display = "none";
-        });
-    }
-};
+let comments = document.getElementsByTagName("ytd-comments")[0];
+
+comments.addEventListener("mousemove");
