e855312d934c4031b005a45a24dc0fac702c64df
olfMombach <olf.mombach@gmx.de>
2020-04-16 19:45:21 +0200

    Changed whole concept

--- content.js
+++ content.js
@@ -5,7 +5,22 @@ img.style.display = "none";
 img.style.position = "absolute";
 img.id = "profilePicMover";
 document.appendChild(img);
+document.addEventListener("load", e => {
+    console.log("attached")
+    let comments = document.getElementsByTagName("ytd-comments")[0];
 
-let comments = document.getElementsByTagName("ytd-comments")[0];
-
-comments.addEventListener("mousemove");
+    comments.addEventListener("mousemove", e => {
+        if (e.target.tagName === "IMG") {
+            console.log("Moving on picture");
+            let mover = document.getElementById("profilePicMover");
+            mover.style.left = e.x + 40 + "px";
+            mover.style.top = e.y + 40 + "px";
+            mover.style.display = "unset";
+        }
+    });
+    comments.addEventListener("mouseleave", e => {
+        console.log("left");
+        let mover = document.getElementById("profilePicMover");
+        mover.style.display = "none";
+    });
+});
