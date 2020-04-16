dafc041f478ddde80c186686b155522e0226c8e5
olfMombach <olf.mombach@gmx.de>
2020-04-17 01:48:16 +0200

    First working version

--- content.js
+++ content.js
@@ -1,26 +1,25 @@
-console.log("Started");
-
 let img = document.createElement("img");
 img.style.display = "none";
 img.style.position = "absolute";
 img.id = "profilePicMover";
-document.appendChild(img);
-document.addEventListener("load", e => {
-    console.log("attached")
+img.src = "http://hoppla1232.bplaced.net/assets/cat2.png";
+img.style.zIndex = "99999";
+document.body.appendChild(img);
+
+window.setTimeout(e => {
     let comments = document.getElementsByTagName("ytd-comments")[0];
 
     comments.addEventListener("mousemove", e => {
         if (e.target.tagName === "IMG") {
-            console.log("Moving on picture");
             let mover = document.getElementById("profilePicMover");
-            mover.style.left = e.x + 40 + "px";
-            mover.style.top = e.y + 40 + "px";
+            mover.src = e.target.src.replace(/^(https.*)(=.*)$/gi, "$1");
+            mover.style.left = e.pageX + 40 + "px";
+            mover.style.top = e.pageY + 40 + "px";
             mover.style.display = "unset";
         }
     });
     comments.addEventListener("mouseleave", e => {
-        console.log("left");
         let mover = document.getElementById("profilePicMover");
         mover.style.display = "none";
     });
-});
+}, 2000);
